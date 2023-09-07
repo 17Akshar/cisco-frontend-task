b79e8c3c21064f0c5d09923309e2ab27507d1684
17Akshar <aksharparmar2002@gmail.com>
2023-09-07 15:41:49 +0530

    IP ADDRESS ADDED

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useState,useEffect } from 'react';
 import './App.css';
 import Banner from './Banner'
 import Exhibit from './Exhibit';
 function App() {
+  const [IP4,setIP4] = useState("none")
+  const [IP6,setIP6] = useState("none")
+  const getData = async() => {
+    const response = await fetch("https://api64.ipify.org?format=json")
+    const result = await response.json()
+    setIP4(result.ip)
+    setIP6(result.ip)
+
+  }
+  useEffect(()=>{
+    getData()
+  },[])
   return (
     <>
     <div className='main'>
@@ -9,8 +22,11 @@ function App() {
     <Banner title = "Sextant Dashboard"></Banner>
     </div>
     <div className='exhibit'>
-      <Exhibit></Exhibit>
-    </div>
+{    (IP4=="none") ? <Exhibit name="Loading.." address="Loading"></Exhibit>:<Exhibit name="IPV4" address={IP4}></Exhibit>
+}  
+{
+  (IP6=="none")?<Exhibit name="Loading.." address="Loading"></Exhibit>:<Exhibit name="IPV6" address={IP6}></Exhibit>
+}        </div>
     </div>
     </>
   );
--- src/Exhibit.js
+++ src/Exhibit.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import './Exhibit.css'
-function Exhibit(){
+function Exhibit(props){
   return (
 
     <>
     <div className='card'>
-      <h1>This is the explicit</h1>
-      <p>this is the explicit para</p>
+      <h1>{props.name}</h1>
+      <p>{props.address}</p>
     </div>
     </>
 
