255464365ab3feb4e3d7cf9c31a791c0e8c0c8f7
17Akshar <aksharparmar2002@gmail.com>
2023-09-07 14:28:12 +0530

    banner and explicit done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Banner from './Banner'
+import Exhibit from './Exhibit';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main'>
+    <div className='Banner'>
+    <Banner title = "Sextant Dashboard"></Banner>
     </div>
+    <div className='exhibit'>
+      <Exhibit></Exhibit>
+    </div>
+    </div>
+    </>
   );
 }
 
---
+++ src/Exhibit.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Exhibit.css'
+function Exhibit(){
+  return (
+
+    <>
+    <div className='card'>
+      <h1>This is the explicit</h1>
+      <p>this is the explicit para</p>
+    </div>
+    </>
+
+  );
+}
+export default Exhibit
